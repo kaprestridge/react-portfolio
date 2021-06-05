f520beefefcdec5ac195240b55e42c508b229600
kaprestridge <kaprestridge@gmail.com>
2021-06-05 18:06:06 +1000

    added project data file and basic styling for project page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,7 @@ import logo from '../img/kp-logo.png'
 const Nav = () => {
     return(
         <>
-        <nav className="h-20 bg-olive text-red-50 flex flex-row flex-wrap items-center ...">
+        <nav className="h-20 bg-olive text-red-50 flex flex-row flex-wrap items-center fixed w-screen">
             <img src={logo} alt="Kathryn's logo" class="h-20"></img>
             <p className="m-2">Projects</p>
             <p className="m-2">Contact</p>
