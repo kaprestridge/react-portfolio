e436510e804948eadd79b0768795452fbf5fcbfa
kaprestridge <kaprestridge@gmail.com>
2021-06-25 18:50:35 +1000

    added modal and contact form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
+import React from 'react';
 //components
 import About from './components/About'
 import Nav from './components/Nav'
 import Projects from './components/Projects'
-import Contact from './components/Contact';
-//img
-// import background from './img/print-bg.png'
+// import Contact from './components/Contact';
+
+
 const App = () => {
+
   return(
     <>
     <div id="background "className="font-mono bg-hero-pattern cursor-default" >
     <Nav />
     <About />
     <Projects />
-    <Contact />
     </div>
     </>
   )
@@ -21,5 +22,3 @@ const App = () => {
 
 export default App;
 
-
-// style={{ backgroundImage: `url(${background})`, resizeMode: 'repeat' }}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,42 @@
 import React from 'react'
 import logo from '../img/kp-logo.png'
 import {Link} from 'react-scroll'
+import Contact from './Contact'
+import Modal from 'react-modal'
 
 const Nav = () => {
+    const [modalIsOpen,setIsOpen] = React.useState(false);
+    const closeModal = () => {
+      setIsOpen(false);
+    }
+  
+    const openModal = () => {
+      setIsOpen(true);
+    }
     return(
         <>
         <nav className="h-20 z-50 bg-olive text-red-50 flex flex-row flex-wrap items-center fixed w-screen">
             <Link to="about" spy={true} smooth={true}>
                 <img src={logo} alt="Kathryn's logo" className="h-20 cursor-pointer"></img>
             </Link>
-            <Link  to="projects" spy={true} smooth={true} className="cursor-pointer">Projects</Link>
-            <p className="m-2 cursor-pointer">Contact</p>
+            <Link  to= "projects" spy={true} smooth={true} className="cursor-pointer">Projects</Link>
+            <Link onClick={openModal} className="m-2 cursor-pointer">Contact</Link>
         </nav>
+        <div className="h-full w-full flex flex-col items-center justify-center">
+        <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Contact"
+        className =" mx-10 md:mx-40 my-40 bg-olive py-20 shadow-2xl rounded-md "
+        >
+            <Contact />
+        </Modal>
+        </div>
     </>
     )
 }
+
+
+
 
 export default Nav
